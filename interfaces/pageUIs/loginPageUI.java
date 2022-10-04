5861d9b7ed4f8bd8f882aba8813ad3c859b4a2a6
hmman3141 <97515333+hmman3141@users.noreply.github.com>
2022-10-04 16:42:19 +0700

    master, init project

---
+++ interfaces/pageUIs/loginPageUI.java
@@ -0,0 +1,5 @@
+package pageUIs;
+
+public class loginPageUI {
+	public static final String EMAIL_TEXTBOX = "";
+}
---
+++ testcases/com/nopcommerce/user/User_02_MyAccount.java
@@ -0,0 +1,19 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_02_MyAccount {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void TC_01() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+}
