8868f5e4fd278726c3322534d40d618bcd74f891
hmman3141 <97515333+hmman3141@users.noreply.github.com>
2022-11-09 21:04:26 +0700

    master, actions_commons BaseTest update set_Browser_Driver

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,22 +4,38 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	public String projectPath = System.getProperty("user.dir");
+	private String cocCocPath = "C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe";
+	private String cocCocDriverVersion = "106.0.5249.61";
+	private String firefoxDriverVersion = "0.31.0";
 
 	protected WebDriver getBrowserDriver(String browserName) {
-		System.out.println(browserName);
 		WebDriver driver = null;
 		switch (browserName) {
-		case "Firefox":
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		case "firefox":
+			WebDriverManager.firefoxdriver().driverVersion(firefoxDriverVersion).setup();
 			driver = new FirefoxDriver();
 			break;
-		case "Chrome":
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
+			break;
+		case "coccoc":
+			WebDriverManager.chromedriver().driverVersion(cocCocDriverVersion).setup();
+			ChromeOptions option = new ChromeOptions();
+			option.setBinary(cocCocPath);
+			driver = new ChromeDriver(option);
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
 			break;
 		default:
 			throw new RuntimeException("Invalid browser name");
