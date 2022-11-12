458ee76e83b5ed29f5338e4df62f04e7e2566e65
hmman3141 <97515333+hmman3141@users.noreply.github.com>
2022-11-12 10:27:05 +0700

    master, testcases_user update User_02_Login add Page_Generator_Manager

---
+++ testcases/com/nopcommerce/user/User_02_Login_03_Page_Generator_Manager.java
@@ -0,0 +1,85 @@
+package com.nopcommerce.user;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import PageObjects.HomePageObject;
+import PageObjects.LoginPageObject;
+import PageObjects.PageGeneratorManager;
+import commons.BaseTest;
+
+public class User_02_Login_03_Page_Generator_Manager extends BaseTest {
+	private WebDriver driver;
+	private LoginPageObject loginPageObject;
+	private HomePageObject homePageObject;
+	private String email, invalidEmail, password;
+
+	@Parameters("Browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		driver.get("https://demo.nopcommerce.com/");
+		homePageObject = PageGeneratorManager.getHomePage(driver);
+		email = getRandomNumber() + "@test.com";
+		invalidEmail = "abc@test/com";
+		password = "password";
+	}
+
+	private int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt();
+	}
+
+	@Test
+	public void TC_01_LoginWithEmptyData() {
+		loginPageObject = homePageObject.clickToLoginLink();
+
+		loginPageObject.clickToLoginButton();
+
+		Assert.assertEquals(loginPageObject.getErrorMessageAtEmailInput(), "Please enter your email");
+	}
+
+	@Test
+	public void TC_02_LoginWithInvalidEmail() {
+		loginPageObject = homePageObject.clickToLoginLink();
+
+		loginPageObject.sendKeyToEmailTextbox(invalidEmail);
+		loginPageObject.clickToLoginButton();
+
+		Assert.assertEquals(loginPageObject.getErrorMessageAtEmailInput(), "Wrong email");
+	}
+
+	@Test
+	public void TC_03_LoginWithNonExistentEmail() {
+		loginPageObject = homePageObject.clickToLoginLink();
+
+		loginPageObject.sendKeyToEmailTextbox(email);
+		loginPageObject.sendKeyToPasswordTextbox(password);
+		loginPageObject.clickToLoginButton();
+
+		Assert.assertEquals(loginPageObject.getErrorMessageByNonExistentEmail(),
+				"Login was unsuccessful. Please correct the errors and try again.\n" + "No customer account found");
+	}
+
+	@Test
+	public void TC_04_LoginWithBlankPassword() {
+		loginPageObject = homePageObject.clickToLoginLink();
+
+		loginPageObject.sendKeyToEmailTextbox(email);
+		loginPageObject.clickToLoginButton();
+
+		Assert.assertEquals(loginPageObject.getErrorMessageByNonExistentEmail(),
+				"Login was unsuccessful. Please correct the errors and try again.\n" + "No customer account found");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+}
